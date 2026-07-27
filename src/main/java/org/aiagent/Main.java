aac6d779b12bd6458c417a098d4d2d02528c317a
gowresh-learning <gowresh.learning@gmail.com>
2026-07-27 22:33:40 +0530

    Initial Playwright setup

---
+++ src/main/java/org/aiagent/Main.java
@@ -0,0 +1,27 @@
+package org.aiagent;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Playwright playwright = Playwright.create()) {
+
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false));
+
+            Page page = browser.newPage();
+
+            page.navigate("https://testautomationpractice.blogspot.com/");
+
+            System.out.println(page.title());
+
+            page.screenshot(new Page.ScreenshotOptions().setPath(java.nio.file.Paths.get("homepage.png")));
+
+            browser.close();
+        }
+    }
+}
